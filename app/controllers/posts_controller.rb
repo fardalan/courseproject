a9cdd64101a5e14c0cdd0192e31cabad094c9119
Firouz Ardalan <ardalan11@gmail.com>
2012-12-27 14:28:09 -0500

    added comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,15 @@ class PostsController < ApplicationController
 
 	def show
 		@post = Post.where(slug: params[:id]).first
+		@comment = Comment.new
 	end
 
+	def new
+		@post = Post.new
+	end
+
+	def create
+		Post.create(params[:post])
+		redirect_to root_path
+	end
 end
